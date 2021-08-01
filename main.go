8274dbeeb732a1357a17b027dbfa799aa01185df
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-01 17:33:06 +0300

    Duplicator v2

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"bufio"
 	"fmt"
@@ -9,13 +8,29 @@ import (
 
 func main(){
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-	for line, n := range counts{
-		if n>1{
-			fmt.Printf("%d\t%s\n", n, line)
+	files := os.Args[1:]
+	fmt.Printf("Length of args %d\n", len(os.Args[1:]))
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _,arg := range files {
+			f,err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr,"dup2: %v\n", err)
+			}
+			countLines(f, counts)
+			f.Close()
 		}
+	}
+	for line,n := range counts {
+		if n>1 {
+			fmt.Printf("%d %s\n", n, line)
+		}
+	}
+}
+func countLines(f *os.File, counts map[string]int){
+	input := bufio.NewScanner(f)
+	for input.Scan(){
+		counts[input.Text()]++
 	}
 }
