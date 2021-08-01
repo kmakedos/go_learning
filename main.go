d3be034b8ba0ef4eb681e6a8d0270f3419473ae3
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-01 17:11:23 +0300

    Deduplicator of lines

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
+
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main(){
-	var s, sep, arg string
-	for _, arg = range(os.Args[1:]){
-		s += sep + arg
-		sep = " "
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
 	}
-	fmt.Println(s)
+	for line, n := range counts{
+		if n>1{
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
-
-
