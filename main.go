ce5c493cce5ba2d05d5bae80885772e5beec3218
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-01 14:05:25 +0300

    Simple echo

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
-	fmt.Println("Hello world\n")
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
 
 
