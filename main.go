2b721fb3556d6a6c1d106f48b420c8396748d326
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-03 13:01:35 +0300

    A simple web server

--- main.go
+++ main.go
@@ -2,41 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
 func main(){
-	start := time.Now()
-	ch := make(chan string)
-	for _,url := range os.Args[1:]{
-		go fetch(url, ch)
-	}
-	for range os.Args[1:]{
-		fmt.Println(<-ch)
-	}
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
-func fetch(url string, ch chan<-string){
-	if !strings.HasPrefix(url, "http") {
-		url = "http://" + url
-	}
-	start := time.Now()
-	resp,err := http.Get(url)
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	nbytes,err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
-		return
-	}
-	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
