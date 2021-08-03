6d293609a3a5d62dd1196db8457f14922315b59a
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-03 12:35:28 +0300

    Concurrently fetch urls

--- main.go
+++ main.go
@@ -3,28 +3,40 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main(){
+	start := time.Now()
+	ch := make(chan string)
 	for _,url := range os.Args[1:]{
-		if !strings.HasPrefix(url, "http") {
-			url = "http://" + url
-		}
-		resp,err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
-			os.Exit(1)
-		}
-		n,err := io.Copy(os.Stdout, resp.Body)
-		resp.Body.Close()
-		if err != nil{
-			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("\nWritten %d bytes\n", n)
-		fmt.Printf("Status Code: %s\n", resp.Status)
+		go fetch(url, ch)
 	}
+	for range os.Args[1:]{
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+func fetch(url string, ch chan<-string){
+	if !strings.HasPrefix(url, "http") {
+		url = "http://" + url
+	}
+	start := time.Now()
+	resp,err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nbytes,err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
