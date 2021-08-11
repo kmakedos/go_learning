b2d9178f804a18dfe00ba44e3a45cb9530217b8c
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-11 22:41:38 +0300

    A smart way to populate a table with values from previous positions, and then calculate bits of numbers given from console

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
-import "go_learning/gui"
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+)
+
+var pc [256]byte
+func init(){
+	for i:= range pc{
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
 
 
 func main(){
-	gui.CallDesktop()
+	flag.Parse()
+	for i := range flag.Args() {
+		number,_ := strconv.ParseUint(flag.Args()[i],10, 64)
+		fmt.Println(PopCount(number))
+	}
+
 }
