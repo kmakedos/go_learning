4d072c151b8668cec723cfc372be16241a3f779f
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-13 18:32:13 +0300

    SVG generation with floating numbers

--- main.go
+++ main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"strconv"
+	"math"
 )
 
-var pc [256]byte
-func init(){
-	for i:= range pc{
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
+const(
+	width, height 	= 600,320
+	cells  			= 100
+	xyrange 		= 30.0
+	xyscale 		= width/2/xyrange
+	zscale 			= height * 0.4
+	angle 			= math.Pi / 6
+)
 
-func PopCount(x uint64) int {
-	var bits int
-	for i := 1; i<8; i++  {
-		bits += int(pc[byte(x >> i)])
-	}
-	return bits
-}
-
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main(){
-	flag.Parse()
-	for i := range flag.Args() {
-		number,_ := strconv.ParseUint(flag.Args()[i],10, 64)
-		fmt.Println(PopCount(number))
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' " +
+		"style='stroke: grey; fill: white; stroke-width: 0.7 ' " +
+		"width='%d' height='%d'>", width, height)
+	for i:=0 ; i<cells; i++ {
+		for j :=0; j< cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
 	}
+	fmt.Println("</svg>")
+}
 
+func corner(i, j int) (float64, float64){
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	z := f(x,y)
+
+	sx := width/2 + (x-y) * cos30 * xyscale
+	sy := height/2 +(x+y)* sin30 * xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x,y float64) float64{
+	r := math.Hypot(x,y)
+	return math.Sin(r)/r
 }
