94350652b0e57f0e19665b0d11806b6683e83807
Kostas Makedos <kostas.makedos@gmail.com>
2021-08-17 21:13:47 +0300

    Slice games

--- main.go
+++ main.go
@@ -1,41 +1,37 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/png"
-	"math/cmplx"
-	"os"
+	"fmt"
 )
 
-func main(){
-	const (
-		xmin,ymin,xmax,ymax = -2,-2,+2,+2
-		width,height 		= 1024,1024
-	)
-
-	img := image.NewRGBA(image.Rect(0,0, width,height))
-	for py :=0 ; py < height; py++{
-		y := float64(py)/height*(ymax-ymin) + ymin
-		for px:= 0; px < width; px++{
-			x:= float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x,y)
-			img.Set(px, py, mandelbrot(z))
-		}
-	}
-	png.Encode(os.Stdout, img)
+func arcopy(x [3]int) [3]int {
+	x[0] = -1
+	return x
 }
 
-func mandelbrot(z complex128) color.Color{
-	const iterations = 255
-	const contrast	 = 15
-	var v complex128
-	for n:=uint8(0); n<iterations; n++{
-		v = v * v + z
-		if cmplx.Abs(v) > 2 {
-			return color.RGBA{128-contrast*n, 128-contrast*n, 127-contrast*n, 255-contrast }
-		}
+func achange(x [3]int) {
+	x[0] = -2
+}
 
+func slicechange(sx []int){
+	sx[0] = -3
+}
+func main(){
+	x := [3]int{1,2,3}
+	for k := range x {
+		fmt.Println( x[k])
 	}
-	return color.RGBA{0, 0,0,0}
+	arcopy(x)
+	for k:=range x{
+		fmt.Println(x[k])
+	}
+	achange(x)
+	for k:=range x{
+		fmt.Println(x[k])
+	}
+	slicechange(x[0:2])
+	for k:= range x{
+		fmt.Println(x[k])
+	}
+
 }
