21045ebc19a0c0a936b2d08cea50ec894c72a3f4
Kostas Makedos <kostas.makedos@gmail.com>
2021-11-01 00:31:03 +0200

    Creating a simple linked list

--- main.go
+++ main.go
@@ -1,45 +1,48 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"time"
-)
+import "fmt"
 
-func fib1(n int) int {
-	if n < 2 {
-		return n
-	} else {
-		return fib1(n-1) + fib1(n-2)
-	}
+type Node struct {
+	id int
+	kids []*Node
+	parent *Node
 }
 
+func createNode(id int)*Node{
+	t := Node{id, nil, nil}
+	return &t
+}
 
-func fib2(n int) int {
-	f := make([]int, n+1)
-	f[0] = 0
-	f[1] = 1
-	for i := 2; i<n+1; i++ {
-		f[i] = f[i-1] + f[i-2]
+func (this *Node)addChild(kid *Node){
+	kid.parent = this
+	this.kids = append(this.kids, kid)
+}
+
+func (this *Node)iterateNodes(){
+	for k,v:=range(this.kids){
+		fmt.Println(k,v.kids)
 	}
-	return f[n]
 }
 
 
 func main(){
-	if len(os.Args) < 2 {
-		log.Fatalln("Not enough arguments, please give me a num")
-	}
-	n,err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		log.Fatalln(err)
-	}
-	start := time.Now()
-	fmt.Println("Fib1:", fib1(n))
-	log.Println("Fib1 took:", time.Since(start))
-	start = time.Now()
-	fmt.Println("Fib2:", fib2(n))
-	fmt.Println("Fib2 took:", time.Since(start))
+	head := createNode(0)
+	t1 := createNode(1)
+	t2 := createNode(2)
+	t3 := createNode(3)
+	t4 := createNode(4)
+	t5 := createNode(5 )
+	head.addChild(t1)
+	head.addChild(t2)
+	t1.addChild(t3)
+	t2.addChild(t3)
+	t3.addChild(t4)
+	t3.addChild(t5)
+
+	head.iterateNodes()
+
+
+
+
+
 }
