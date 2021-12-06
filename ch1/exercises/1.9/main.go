2354e582dda4afe6e6b777e10705dd1f180d1f5d
Kostas Makedos <kostas.makedos@gmail.com>
2021-12-07 00:34:35 +0200

    Adding exercises 1.9

---
+++ ch1/exercises/1.9/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	urls := []string{
+		"http://www.in.gr",
+		"https://www.wikipedia.org",
+		"https://9gag.com",
+	}
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range urls {
+		log.Printf("URL: %s\n", url)
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
